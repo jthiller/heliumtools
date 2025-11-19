4479573fe631bdb5b1588c3e95ca227172cb84bb
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2025-11-19 09:58:15 -0800

    localize fonts

--- pages/public/src/main.jsx
+++ pages/public/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Landing from "./pages/Landing.jsx";
+import "../fonts/inter.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- pages/public/vite.config.js
+++ pages/public/vite.config.js
@@ -5,46 +5,10 @@ import { fileURLToPath } from "node:url";
 
 const root = fileURLToPath(new URL(".", import.meta.url));
 
-const htmlInjectionPlugin = () => {
-  return {
-    name: "html-injection",
-    transformIndexHtml(html) {
-      return {
-        html,
-        tags: [
-          {
-            tag: "link",
-            attrs: {
-              rel: "preconnect",
-              href: "https://fonts.googleapis.com",
-            },
-            injectTo: "head",
-          },
-          {
-            tag: "link",
-            attrs: {
-              rel: "preconnect",
-              href: "https://fonts.gstatic.com",
-              crossorigin: true,
-            },
-            injectTo: "head",
-          },
-          {
-            tag: "link",
-            attrs: {
-              href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-              rel: "stylesheet",
-            },
-            injectTo: "head",
-          },
-        ],
-      };
-    },
-  };
-};
+
 
 export default defineConfig({
-  plugins: [react(), htmlInjectionPlugin()],
+  plugins: [react()],
   base: "/",
   build: {
     rollupOptions: {
