13b2acd9488db2a7672ee7b0e592d90ad7d00b97
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2025-11-18 22:44:21 -0800

    Switch to Google Fonts to use optimized distribution

--- pages/public/vite.config.js
+++ pages/public/vite.config.js
@@ -5,8 +5,46 @@ import { fileURLToPath } from "node:url";
 
 const root = fileURLToPath(new URL(".", import.meta.url));
 
+const htmlInjectionPlugin = () => {
+  return {
+    name: "html-injection",
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: "link",
+            attrs: {
+              rel: "preconnect",
+              href: "https://fonts.googleapis.com",
+            },
+            injectTo: "head",
+          },
+          {
+            tag: "link",
+            attrs: {
+              rel: "preconnect",
+              href: "https://fonts.gstatic.com",
+              crossorigin: true,
+            },
+            injectTo: "head",
+          },
+          {
+            tag: "link",
+            attrs: {
+              href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+              rel: "stylesheet",
+            },
+            injectTo: "head",
+          },
+        ],
+      };
+    },
+  };
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), htmlInjectionPlugin()],
   base: "/",
   build: {
     rollupOptions: {
