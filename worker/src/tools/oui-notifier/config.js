65b61b449268fe73f6fafbca7901ed0d6cc74099
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2025-12-05 16:59:26 -0800

    Fix float munging of DC floor

--- worker/src/tools/oui-notifier/config.js
+++ worker/src/tools/oui-notifier/config.js
@@ -1,7 +1,7 @@
 export const DC_TO_USD_RATE = 0.00001;
 export const BURN_LOOKBACK_DAYS = 7;
 export const ZERO_BALANCE_USD = 35;
-export const ZERO_BALANCE_DC = ZERO_BALANCE_USD / DC_TO_USD_RATE;
+export const ZERO_BALANCE_DC = 3500000; // $35 worth of DC (DC are always whole numbers)
 export const OUI_API_URL = "https://entities.nft.helium.io/v2/oui/all";
 export const BALANCE_HISTORY_DAYS = 30;
 // Avoid Cloudflare subrequest limits when seeding balances; we fetch at most this many per update call.
